c8e6dad7311adb3cd7eb50ea6a323cba7e193908
Azim <Budazimbud>
2026-02-12 10:41:28 +0800

    fixing model service

--- models/service.go
+++ models/service.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -10,8 +12,8 @@ type Service struct {
 	Title       string         `gorm:"type:varchar(255);not null" json:"title"`     
 	Description string         `gorm:"type:text;not null" json:"description"`       
 	Color       string         `gorm:"type:varchar(50);not null" json:"color"`       
-	CreatedAt   gorm.DeletedAt `json:"created_at"`                                  
-	UpdatedAt   gorm.DeletedAt `json:"updated_at"`                                   
+	CreatedAt   time.Time      `json:"created_at"`                                  
+	UpdatedAt   time.Time      `json:"updated_at"`                                   
 	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at"`          
-	IsDeleted bool   `json:"is_deleted" gorm:"default:false"`
+	IsDeleted   bool           `json:"is_deleted" gorm:"default:false"`
 }
